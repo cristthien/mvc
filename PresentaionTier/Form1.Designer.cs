1d3ad67f01f4c229ed0dab1026d30054aca96a75
Nguyen Thien <cristhuuthien@gmail.com>
2023-12-08 09:29:29 +0700

    Strach model

---
+++ PresentaionTier/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace PresentaionTier
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstDanhMuc = new System.Windows.Forms.ListBox();
+            this.btnXem = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstDanhMuc
+            // 
+            this.lstDanhMuc.FormattingEnabled = true;
+            this.lstDanhMuc.Location = new System.Drawing.Point(12, 12);
+            this.lstDanhMuc.Name = "lstDanhMuc";
+            this.lstDanhMuc.Size = new System.Drawing.Size(765, 316);
+            this.lstDanhMuc.TabIndex = 0;
+            // 
+            // btnXem
+            // 
+            this.btnXem.Location = new System.Drawing.Point(12, 357);
+            this.btnXem.Name = "btnXem";
+            this.btnXem.Size = new System.Drawing.Size(209, 42);
+            this.btnXem.TabIndex = 1;
+            this.btnXem.Text = "Xem Danh Sach San Pham";
+            this.btnXem.UseVisualStyleBackColor = true;
+            this.btnXem.Click += new System.EventHandler(this.btnXem_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(305, 357);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(219, 42);
+            this.btnXoa.TabIndex = 2;
+            this.btnXoa.Text = "Xoa Danh Sach San Pham";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(581, 357);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(196, 42);
+            this.btnThoat.TabIndex = 3;
+            this.btnThoat.Text = "Thoat Chuong Trinh";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnXem);
+            this.Controls.Add(this.lstDanhMuc);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstDanhMuc;
+        private System.Windows.Forms.Button btnXem;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnThoat;
+    }
+}
+
---
+++ PresentaionTier/SanPhamForm.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BusinessLogicTier;
+using DTO;
+
+namespace PresentaionTier
+{
+    public partial class SanPhamForm : Form
+    {
+        DanhMucBUS objDM = new DanhMucBUS();
+        SanPhamBUS objSP = new SanPhamBUS();
+        DataTable tbSP;
+        public SanPhamForm()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void SanPhamForm_Load(object sender, EventArgs e)
+        {
+            string strMaDM = Form1.strMaDanhMuc_Chon;
+            tbSP = objSP.GetSanPhamByMADM(strMaDM);
+            dataGridView1.DataSource = tbSP;
+
+            cbDanhMuc.DataSource = objDM.GetDanhMuc();
+            cbDanhMuc.DisplayMember = "TenDM";
+            cbDanhMuc.ValueMember = "MaDM";
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            string strMasp = "";
+            if ((strMasp = dataGridView1.CurrentRow.Cells[0].Value.ToString()) != "")
+            {
+                SanPham sp = objSP.GetSanPhamByMASP(strMasp);
+                txtMaSP.Text = sp.MaSanPham;
+                txtTenSP.Text = sp.TenSanPham;
+                txtSoLuong.Text = sp.SoLuong.ToString();
+                txtGia.Text = sp.DonGia.ToString();
+                txtXuatXu.Text = sp.XuatXu;
+                cbDanhMuc.SelectedValue = sp.MaDanhMuc;
+
+              
+            }
+
+                
+        }
+
+
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+           try {
+                int sl = Convert.ToInt32(txtSoLuong.Text);
+                int dg = Convert.ToInt32(txtGia.Text);
+                SanPham sp = new SanPham(txtMaSP.Text,txtTenSP.Text, sl, dg, txtXuatXu.Text, cbDanhMuc.SelectedValue.ToString() );
+
+                if(objSP.AddSanPham(sp)) {
+                    MessageBox.Show("Them mot san pham thanh con ");
+                    dataGridView1.DataSource = objSP.GetSanPhamByMADM(Form1.strMaDanhMuc_Chon);
+                    
+                }
+                else
+                {
+                    MessageBox.Show("Co loi trong qua trinh them san pham");
+
+                }
+            }
+            catch (Exception ex ) {
+                MessageBox.Show("Co loi trong qua trinh them san pham");
+            }
+        }
+        
+        private void ResetTextFields()
+        {
+            txtGia.Text = "";
+            txtMaSP.Text = "";
+           txtTenSP.Text = "";
+           txtSoLuong.Text = "";
+            txtXuatXu.Text = "";
+            cbDanhMuc.SelectedIndex = -1;
+        }
+
+        private void btnTroVe_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnCapNhap_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int sl = Convert.ToInt32(txtSoLuong.Text);
+                int dg = Convert.ToInt32(txtGia.Text);
+                SanPham sp = new SanPham(txtMaSP.Text, txtTenSP.Text, sl, dg, txtXuatXu.Text, cbDanhMuc.SelectedValue.ToString());
+                if (objSP.UpdateSanPham(sp))
+                {
+                    MessageBox.Show("Da thay doi thanh cong");
+                    dataGridView1.DataSource = objSP.GetSanPhamByMADM(Form1.strMaDanhMuc_Chon);
+                    ResetTextFields();
+
+
+                }
+                else
+                {
+                    MessageBox.Show("Co loi trong qua trinh luu san pham");
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Co loi trong qua trinh luuw san pham");
+            }
+        }
+    }
+
+}
